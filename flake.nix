fcc690750ff0819d445f3202524939dad659442f
David Reed <david@ktema.org>
2025-01-24 16:01:42 -0700

    Working overlayfs implementation

--- flake.nix
+++ flake.nix
@@ -1,101 +1,76 @@
 {
-  description = "Minimal NixOS installation media";
+  # Heavily inspired by https://gist.github.com/FlakM/0535b8aa7efec56906c5ab5e32580adf
+  description = "Flake for Derby Scoreboard";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.scoreboard = with import nixpkgs { system = "x86_64-linux"; };
-let
-  desktopItem = makeDesktopItem {
-    desktopName = "Derby Scoreboard";
-    genericName = "Roller derby scoreboard";
-    categories = [ "Utility" ];
-    exec = "derby-scoreboard";
-    name = "derby-scoreboard";
-  };
-in
-stdenv.mkDerivation rec {
-  pname = "derby-scoreboard";
-  version = "2023.4";
-
-  src = fetchFromGitHub {
-    owner = "davidmreed";
-    repo = "scoreboard";
-    rev = "be68825a5af7800ae3c9f5c78ca6fb9dbd83e335";
-    sha256 = "sha256-l1EdrOqP+nPh2Q6cVkB9lOC4iGDLDigBj08an/BBu9o=";
-    # The Ant scripts for Scoreboard require a full Git checkout.
-    leaveDotGit = true;
-  };
-  buildInputs = [ jre ];
-  nativeBuildInputs = [ copyDesktopItems jdk ant git unzip makeWrapper ];
-  buildPhase = ''
-    runHook preBuild
-    ant zip
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin
-
-    unzip release/crg-scoreboard_*.zip
-    mv crg-scoreboard_*/* $out
-    makeWrapper ${jre}/bin/java $out/bin/derby-scoreboard \
-      --chdir $out \
-      --add-flags "-jar $out/lib/crg-scoreboard.jar" \
-      --add-flags "-Done-jar.silent=true" \
-      --add-flags "-Dorg.eclipse.jetty.server.LEVEL=WARN" \
-      --add-flags "--config-path=$(echo ~/.config/derby-scoreboard)"
-
-    runHook postInstall
-  '';
-
-  desktopItems = [ desktopItem ];
-
-  meta = with lib; {
-    homepage = "https://github.com/rollerderby/scoreboard";
-    description = "Roller derby scoreboard";
-  };
-};
-    nixosConfigurations = {
-      exampleIso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          ({ pkgs, ... }: {
-            environment.systemPackages = [  ];
-              # EFI booting
-          isoImage.makeEfiBootable = true;
-
-          # USB booting
-          isoImage.makeUsbBootable = true;
-
-          isoImage.storeContents = [ ];
-          services.xserver.desktopManager.xfce = {
-            enable = true;
-            enableScreensaver = false;
-          };
-
-          services.xserver.displayManager = {
-            gdm = {
-              enable = true;
-              # autoSuspend makes the machine automatically suspend after inactivity.
-              # It's possible someone could/try to ssh'd into the machine and obviously
-              # have issues because it's inactive.
-              # See:
-              # * https://github.com/NixOS/nixpkgs/pull/63790
-              # * https://gitlab.gnome.org/GNOME/gnome-control-center/issues/22
-              autoSuspend = false;
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { system = system; };
+      in
+      rec {
+        formatter.${system} = pkgs.nixfmt-rfc-style;
+        packages.default = pkgs.callPackage ./scoreboard.nix {};
+        nixosConfigurations = {
+          scoreboard = nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs.inputs = inputs;
+            modules = [
+            {
+            users.groups.admin = { };
+            users.groups.sbo = { };
+            users.users = {
+              admin = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" ];
+                password = "admin";
+                group = "admin";
+              };
+              sbo = {
+                isNormalUser = true;
+                password = "";
+                group = "sbo";
+              };
             };
-            autoLogin = {
-              enable = true;
-              user = "nixos";
+            virtualisation.vmVariant = {
+              virtualisation = {
+                memorySize = 2048;
+                cores = 2;
+                graphics = true;
+              };
             };
+            services.xserver = {
+              enable = true;
+              desktopManager = {
+                xfce.enable = true;
+              };
+              displayManager = {
+                    autoLogin.enable = true;
+                    autoLogin.user = "sbo";
+                  defaultSession = "xfce";
+                  lightdm = {
+                    enable = true;
+                };
+                };
+            };
+          }];
           };
-          })
-        ];
-      };
-    };
-  };
+        };
+        apps = rec {
+          default = scoreboard-vm;
+          scoreboard-vm = {
+            type = "app";
+            program = "${nixosConfigurations.scoreboard.config.system.build.vm}/bin/run-nixos-vm";
+          };
+        };
+      }
+    );
 }
--- scoreboard.nix
+++ scoreboard.nix
@@ -1,27 +1,18 @@
-{ lib, stdenv, fetchFromGitHub, unzip, jre, jdk, ant, git, makeWrapper, makeDesktopItem, copyDesktopItems, ... }:
+{ lib, stdenv, fetchFromGitHub, unzip, jre, jdk, ant, git, bash, fuse-overlayfs, xdg-utils, makeWrapper, makeDesktopItem, copyDesktopItems, ... }:
 
-let
-  desktopItem = makeDesktopItem {
-    desktopName = "Derby Scoreboard";
-    genericName = "Roller derby scoreboard";
-    categories = [ "Utility" ];
-    exec = "derby-scoreboard";
-    name = "derby-scoreboard";
-  };
-in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation  {
   pname = "derby-scoreboard";
-  version = "2023.4";
+  version = "2025.5";
 
   src = fetchFromGitHub {
-    owner = "davidmreed";
+    owner = "rollerderby";
     repo = "scoreboard";
-    rev = "be68825a5af7800ae3c9f5c78ca6fb9dbd83e335";
-    sha256 = "sha256-l1EdrOqP+nPh2Q6cVkB9lOC4iGDLDigBj08an/BBu9o=";
+    rev = "v2025.5";
+    sha256 = "sha256-C+oeGKiqfOTWMefOfarn7bsGhXWnpuAG2ukpoA5akxE=";
     # The Ant scripts for Scoreboard require a full Git checkout.
     leaveDotGit = true;
   };
-  buildInputs = [ jre ];
+  buildInputs = [ jre fuse-overlayfs bash xdg-utils ];
   nativeBuildInputs = [ copyDesktopItems jdk ant git unzip makeWrapper ];
   buildPhase = ''
     runHook preBuild
@@ -33,23 +24,94 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     mkdir -p $out/bin
+    mkdir -p $out/dist
 
     unzip release/crg-scoreboard_*.zip
-    mv crg-scoreboard_*/* $out
-    makeWrapper ${jre}/bin/java $out/bin/derby-scoreboard \
-      --chdir $out \
-      --add-flags "-jar $out/lib/crg-scoreboard.jar" \
-      --add-flags "-Done-jar.silent=true" \
-      --add-flags "-Dorg.eclipse.jetty.server.LEVEL=WARN" \
-      --add-flags "--config-path=$(echo ~/.config/derby-scoreboard)"
+    mv crg-scoreboard_*/* $out/dist
 
+    cat > $out/bin/derby-scoreboard <<EOF
+    #!${bash}/bin/bash
+    set -euo pipefail
+
+    # Set up directories we need
+    ROOT_DIR=\$(mktemp -d)
+    CONFIG_DIR=\$(realpath ~/.config/)/derby-scoreboard
+    CACHE_DIR=\$(realpath ~/.cache/)/derby-scoreboard
+    if [ ! -d "\$CONFIG_DIR" ]; then
+        mkdir -p "\$CONFIG_DIR"
+    fi
+    if [ ! -d "\$CACHE_DIR" ]; then
+        mkdir -p "\$CACHE_DIR"
+    fi
+    WORK_DIR=\$(mktemp -d "\$CACHE_DIR/workXXXXXXX")
+
+    # Clean up if a signal is thrown/error occurs
+    cleanup()
+    {
+        if [[ -v SCOREBOARD_PID ]]; then
+            kill \$SCOREBOARD_PID || true
+        fi
+        if [[ -v FUSE_PID ]]; then
+            kill \$FUSE_PID || true
+        fi
+        if [ -d \$WORK_DIR ]; then
+            rm -rf \$WORK_DIR || true
+        fi
+        if [ -d \$ROOT_DIR ]; then
+            rm -rf \$ROOT_DIR || true
+        fi
+    }
+
+    trap cleanup 1 2 3 6 15 EXIT
+
+    # Start the FUSE overlay filesystem
+    ${fuse-overlayfs}/bin/fuse-overlayfs \
+      -o lowerdir=$out/dist \
+      -o upperdir=\$CONFIG_DIR \
+      -o workdir=\$WORK_DIR \
+      -o squash_to_uid=\$(id -u) \
+      -f true \
+      \$ROOT_DIR &
+    FUSE_PID=\$!
+
+    # Wait for the FUSE filesystem to be ready
+    timeout 30 bash -c "until stat \$ROOT_DIR/lib/crg-scoreboard.jar; do sleep 0.5; done"
+
+    # Start the scoreboard. Working directory MUST be the root dir.
+    cd \$ROOT_DIR
+    ${jre}/bin/java -jar lib/crg-scoreboard.jar \
+      -Done-jar.silent=true \
+      -Dorg.eclipse.jetty.server.LEVEL=WARN &
+    SCOREBOARD_PID=\$!
+
+    # Wait for the scoreboard to be ready
+    timeout 30 bash -c 'until echo > /dev/tcp/localhost/8000; do sleep 0.5; done'
+
+    xdg-open "http://localhost:8000"
+
+    # Wait until (1) the user terminates us or
+    # (2) either the scoreboard or FUSE stops.
+    wait -n \$SCOREBOARD_PID \$FUSE_PID
+
+    # Tear down any remaining processes and delete temp dirs.
+    cleanup
+    EOF
+
+    chmod +x $out/bin/derby-scoreboard
     runHook postInstall
   '';
 
-  desktopItems = [ desktopItem ];
+  desktopItems = let item = makeDesktopItem {
+    desktopName = "CRG Derby Scoreboard";
+    genericName = "Roller derby scoreboard";
+    categories = [ "Utility" ];
+    exec = "derby-scoreboard";
+    name = "derby-scoreboard";
+  }; in [ item ];
 
   meta = with lib; {
     homepage = "https://github.com/rollerderby/scoreboard";
     description = "Roller derby scoreboard";
+    license = [licenses.gpl3Plus licenses.asl20];
   };
 }
